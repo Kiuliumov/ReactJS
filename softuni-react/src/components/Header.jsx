88fae85c1113d32c75742cb965ed978c4d25fa6a
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-12 18:48:38 +0300

    Update Header.jsx

--- softuni-react/src/components/Header.jsx
+++ softuni-react/src/components/Header.jsx
@@ -1,10 +1,18 @@
-function Header(){
-    
-    return (
-        <header>
-        <h1> My React App</h1>
-        </header>
-    );
+function Header(props) {
+  let display;
+
+  if (props.name) {
+    display = props.name;
+  } else {
+    display = "Guest";
+  }
+
+  return (
+    <header>
+      <h1>My React App</h1>
+      <h2>Welcome, {display}!</h2>
+    </header>
+  );
 }
 
 export default Header;
