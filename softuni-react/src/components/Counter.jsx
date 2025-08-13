d740890250e167242171740d0d6e05714776f1b8
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-08-13 08:09:11 +0300

    Update Counter.jsx

--- softuni-react/src/components/Counter.jsx
+++ softuni-react/src/components/Counter.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 
 export default function Counter(){
     const [count, setCount] = useState(0);
-    
+    if(count > 20){
+        return <div>Count is greater than 20</div>;
+    }
     return (
         <button onClick={() => setCount(count + 1)}>Clicks: {count}</button>
     )
